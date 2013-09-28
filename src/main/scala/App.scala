c2e5d73e266bcae0fd4aa4567a458e83aba3f015
Torgeir Thoresen <torgeir.thoresen@gmail.com>
2013-09-28 22:51:28 +0200

    Updating case class members with .copy().

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,4 +1,5 @@
 import configuration.MongoConfigMorphia
+import gd.wa.User
 import org.springframework.scala.context.function.FunctionalConfigApplicationContext
 import services.UserService
 
@@ -24,6 +25,13 @@ object App {
     val user2 = userService.findUserByUsername(email)
     println("equal users", user == user2, user.get, user2.get)
 
-    println(userService.findUserNumber(42))
+    val user42: Option[User] = userService.findUserNumber(42)
+    println(user42)
+
+    val updatedUser42: Option[User] = user42.map {
+      u => userService.updateNameForUser(s"${u.name} with new name", u)
+    }
+    println(updatedUser42)
+
   }
 }
--- src/main/scala/services/UserService.scala
+++ src/main/scala/services/UserService.scala
@@ -8,18 +8,18 @@ class UserService(repo: UserRepository) extends Service[User](repo) {
   import com.mysema.query.scala.Helpers._
   import gd.wa.{QUser => user}
 
-  def createUser(username: String, name: String): User = {
-    findUserByUsername(username) match {
-      case Some(user) => {
-        logger.info(s"User with username $username already exists, returning existing one")
-        user
-      }
-      case None => {
-        logger.info(s"Creating user with username $username")
-        repo.save(User(username, name))
-      }
+  def createUser(username: String, name: String): User = findUserByUsername(username) match {
+    case Some(user) => {
+      logger.info(s"User with username $username already exists, returning existing one")
+      user
+    }
+    case None => {
+      logger.info(s"Creating user with username $username")
+      repo.save(User(username, name))
     }
   }
+
+  def updateNameForUser(newName: String, user: User) = repo.save(user.copy(name = newName))
 
   def findUserByUsername(usernameToFind: String) = repo.query.where(user.username.eq(usernameToFind)).single
 
